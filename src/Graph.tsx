bc5dd0ee86d0e9cb1caef2cb74add40ba0e55d1c
Ifigeneia Manolou <ifigeneiamanolou26@gmail.com>
2023-08-19 12:27:09 +0300

    newapp

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
+import { timeStamp } from 'console';
 
 /**
  * Props declaration for <Graph />
@@ -14,9 +15,10 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
+
 
 /**
  * React component that renders Perspective based on data
@@ -32,7 +34,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -49,10 +51,19 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
-    }
-  }
+      elem.setAttribute ('view','y_line')
+      elem.setAttribute ('column-pivots', '["stock"]')
+      elem.setAttribute ('row-pivots', '["timestamp"]')
+      elem.setAttribute ('columns', '["top_ask_prize"]')
+      // avoid repetition
+      elem.setAttribute ('aggregates', `
+        {"stock":"distinct count",
+        "top_ask_price":"avg",
+        "top_bid_price":"avg",
+        "timestamp":"distinct count" } `);
+    } 
 
-  componentDidUpdate() {
+  this.componentDidUpdate() {
     // Everytime the data props is updated, insert the data into Perspective table
     if (this.table) {
       // As part of the task, you need to fix the way we update the data props to
